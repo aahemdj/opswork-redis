7d0319a2e55bb102d68d176ddfc25f06ce5a0732
Safdar Imam <safdar.imam@tenpearls.com>
2015-04-24 17:45:48 +0500

    test

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,29 +1,57 @@
 #
+
 # Cookbook Name:: redis
+
 # Recipe:: default
+
 #
+
 # Copyright 2015, YOUR_COMPANY_NAME
+
 #
+
 # All rights reserved - Do Not Redistribute
+
 #
-execute "download" do
-   command "wget http://download.redis.io/releases/redis-3.0.0.tar.gz -O /home/ec2-user/redis.tar.gz"
-end
-
-execute "extract-tar" do
-  command "tar -xvf /home/ec2-user/redis.tar.gz"
-end
 
 
-execute "Cd" do
-  command "cd /home/ec2-user/redis-*"
+
+
+
+#execute "download" do
+
+#   command "wget http://download.redis.io/releases/redis-3.0.0.tar.gz -O /home/hamza/redis-3.0.0.tar.gz"
+
+#end
+
+
+
+bash "install_redis" do
+
+  user 'root'
+
+  cwd "/home/ec2-user"
+
+  code <<-EOH
+
+  wget http://download.redis.io/releases/redis-3.0.0.tar.gz
+
+  tar -xvf redis-3.0.0.tar.gz
+
+  cd redis-3.0.0/
+
+  make install
+
+  EOH
+
 end
 
-execute "Make Redis" do
-  command "make install"
-end
+
 
 execute "Start Redis Server" do
-  command "nohup redis-server &"
+
+  command "redis-server &"
+
 end
+
 
